9b32d9371ce041533d2334b326b79e176a74c81e
AhmedSalemsayed <ahmedtaco1996@gmail.com>
2024-09-14 15:46:35 +0300

    allow dark mode preference from O.S

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -5,26 +5,30 @@ import { useLocalStorageState } from "../hooks/useLocalStorageState";
 const DarkModeContext = createContext();
 
 function DarkModeProvider({ children }) {
-  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "isDarkMode");
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(
+    window.matchMedia("(prefers-color-scheme:dark)").matches,
+    "isDarkMode"
+  );
 
   function toggleDarkMode() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
-  useEffect(function(){
-    if(isDarkMode){
-        document.documentElement.classList.add('dark-mode')
-        document.documentElement.classList.remove('light-mode')
-    }else{
+  useEffect(
+    function () {
+      if (isDarkMode) {
+        document.documentElement.classList.add("dark-mode");
+        document.documentElement.classList.remove("light-mode");
+      } else {
         document.documentElement.classList.add("light-mode");
         document.documentElement.classList.remove("dark-mode");
-    }
-
-  },[isDarkMode])
-
+      }
+    },
+    [isDarkMode]
+  );
 
   return (
-    <DarkModeContext.Provider value={{isDarkMode, toggleDarkMode}}>
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
       {children}
     </DarkModeContext.Provider>
   );
